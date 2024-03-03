c1a55c26764c91ba0ebdd190334a3088622c1538
TomokiIkegami <gampsocleis.ussuriensis@gmail.com>
2024-03-03 16:25:08 +0900

    プロジェクトページのURLを修正しました（アプリ名より下のall_projectsの文字を削除）

--- proj_overview/urls.py
+++ proj_overview/urls.py
@@ -3,9 +3,7 @@ from . import views
 app_name = 'proj_overview' # 各ルートがどのアプリケーションのビューなのかを指定
 urlpatterns = [
     # proj_overviewのURLの場合、proj_overviewのurls.pyを呼び出す
-    path('all_projects',views.showAllProjects,name='all_projects'),
-    # 何も指定しない場合、top_pageにアクセス
-    path('',views.top_page, name='top_page'),
+    path('',views.showAllProjects,name='all_projects'), # メモ：第1引数の文字列を削除すると、URLがアプリ名だけになる（逆に文字を入れるとURLを設定できる）
     # proj_overviewの後に整数型のidが来たら、views.pyのdetail関数を呼び出す
     path('<int:id>', views.detail, name='detail')
 ]
--- proj_overview/views.py
+++ proj_overview/views.py
@@ -13,7 +13,10 @@ def showAllProjects(request):
     return render(request, 'proj_overview/all_projects.html',context)   
 
 def top_page(request):
-    return render(request,'proj_overview/top_page.html')
+    context = {}
+    all_article = ArticleInfo.objects.all().order_by('project_start_date').reverse() # 全記事を取得
+    context = {'all_article':all_article,}  # contextに全記事を格納
+    return render(request,'proj_overview/all_projects.html',context)
 
 def detail(request, id):    # ルートで指定した値をid変数として取り出す
     article = get_object_or_404(ArticleInfo, pk=id) # 指定したidのレコードに対応するオブジェクトを取得（pk=primary key、データを識別するための目印）
