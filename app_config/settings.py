bc35ad66468506f5eff43fe4ebe8b8deadecd2e9
TomokiIkegami <gampsocleis.ussuriensis@gmail.com>
2024-03-03 12:13:31 +0900

    環境変数を使うためのモジュールをインポート

--- app_config/settings.py
+++ app_config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv # To use vertual environment
+load_dotenv(verbose=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
